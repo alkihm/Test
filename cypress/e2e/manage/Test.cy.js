25d70f4d31be269d9ea76cc6fe9159541cabefa8
alkihm <alfredkimuyu@gmail.com>
2024-03-27 01:42:57 +0300

    Cards

--- cypress/e2e/manage/Test.cy.js
+++ cypress/e2e/manage/Test.cy.js
@@ -1,6 +1,7 @@
 describe('I&M QA Technical Test', () => {
     beforeEach(() => {
         cy.viewport(1440, 900);
+        cy.visit('https://www.imbankgroup.com/ke/')
         Cypress.on("uncaught:exception", (err, Runnable) => {
           return false;
         });
@@ -8,7 +9,6 @@ describe('I&M QA Technical Test', () => {
       });
 
     it('Should navigate through all headers', () => {
-      cy.visit('https://www.imbankgroup.com/ke/')
       cy.get('#menu-item-12094').click()
       cy.get('[href="https://www.imbankgroup.com/ke/business/"]').click()
       cy.get('#menu-item-12093 > [href="https://www.imbankgroup.com/ke/diaspora/"]').click()
@@ -17,4 +17,11 @@ describe('I&M QA Technical Test', () => {
       cy.get('[href="https://www.imbankgroup.com/ke/business/"]').click()
     
     })
+
+    it.only('Should navigate Credit Card Menu', () => {
+        cy.contains("Cards").click({force: true})
+        cy.get(':nth-child(1) > .feature-6-list-item-content > .feature-6-list-item-content-in > .feature-6-list-item-text').click()
+        cy.get(':nth-child(6) > .feature-30-body-col > .card > .text-gray-5 > .card-body > .card-type-22-title > .mx-height-content-holder').click()
+      
+      })
   })
