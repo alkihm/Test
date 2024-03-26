6fa7ded63c1b57022091eba2c95e222130aac432
alkihm <alfredkimuyu@gmail.com>
2024-03-27 01:19:18 +0300

    navigation automation

--- cypress/e2e/manage/Test.cy.js
+++ cypress/e2e/manage/Test.cy.js
@@ -1,6 +1,20 @@
-describe('My First Test', () => {
-    it('Visits the Cypress website', () => {
-      cy.visit('https://www.cypress.io/')
-      cy.contains('Download').click()
+describe('I&M QA Technical Test', () => {
+    beforeEach(() => {
+        cy.viewport(1440, 900);
+        Cypress.on("uncaught:exception", (err, Runnable) => {
+          return false;
+        });
+       
+      });
+
+    it('Should navigate through all headers', () => {
+      cy.visit('https://www.imbankgroup.com/ke/')
+      cy.get('#menu-item-12094').click()
+      cy.get('[href="https://www.imbankgroup.com/ke/business/"]').click()
+      cy.get('#menu-item-12093 > [href="https://www.imbankgroup.com/ke/diaspora/"]').click()
+      cy.get('#menu-item-12100 > :nth-child(1)').click()
+      cy.get('#menu-item-12092').click()
+      cy.get('[href="https://www.imbankgroup.com/ke/business/"]').click()
+    
     })
   })
